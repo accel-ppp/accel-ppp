4a64ade9018c7ce797bb81ce4d7d37ca914b9d10
Gabriel Jeanneau <gabriel.jeanneau@6wind.com>
2023-04-07 15:09:54 +0200

    triton: add list_for_each_entry_safe
    add list_for_each_entry_safe in order to itirate over a list of a given
    type safe against removal in order to free list elements one by one.
    
    Signed-off-by: Gabriel Jeanneau <gabriel.jeanneau@6wind.com>

--- accel-pppd/triton/list.h
+++ accel-pppd/triton/list.h
@@ -278,6 +278,20 @@ static inline void list_splice_init(struct list_head *list,
 	     pos = list_entry(pos->member.next, typeof(*pos), member),	\
 		     prefetch(pos->member.next))
 
+/**
+ * list_for_each_entry	-	iterate over list of given type safe against removal of list entry
+ * @pos:	the type * to use as a loop counter.
+ * @n:		another &struct list_head to use as temporary storage
+ * @head:	the head for your list.
+ * @member:	the name of the list_struct within the struct.
+ */
+#define list_for_each_entry_safe(pos, n, head, member)				\
+	for (pos = list_entry((head)->next, typeof(*pos), member),	\
+		     n = pos->member.next;			\
+	     &pos->member != (head);					\
+	     pos = list_entry(n, typeof(*pos), member),	\
+		     n = pos->member.next)
+
 //#endif /* __KERNEL__ || _LVM_H_INCLUDE */
 
 #endif
