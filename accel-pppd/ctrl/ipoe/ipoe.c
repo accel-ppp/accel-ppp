ae73c2e1ac2ae969ba3f5f402217f044dc882984
Louis Scalbert <louis.scalbert@6wind.com>
2023-06-23 11:21:30 +0200

    ipoe,libnetlink: get vrf information
    Get Linux VRF information from Netlink and store it into the IPOE server
    structure.
    
    Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

--- accel-pppd/ctrl/ipoe/ipoe.c
+++ accel-pppd/ctrl/ipoe/ipoe.c
@@ -2999,6 +2999,9 @@ static void add_interface(const char *ifname, int ifindex, const char *opt, int 
 	int opt_check_mac_change = conf_check_mac_change;
 	struct ifreq ifr;
 	uint8_t hwaddr[ETH_ALEN];
+#ifdef HAVE_VRF
+	char *vrf_name = NULL;
+#endif /* HAVE_VRF */
 
 	str0 = strchr(opt, ',');
 	if (str0) {
@@ -3125,7 +3128,16 @@ static void add_interface(const char *ifname, int ifindex, const char *opt, int 
 
 		serv->active = 1;
 		serv->ifindex = ifindex;
-
+#ifdef HAVE_VRF
+		serv->vrf_ifindex = iplink_get_vrf_ifindex(ifindex);
+		if (serv->vrf_ifindex) {
+			iplink_get_vrf_info(serv->vrf_ifindex, &vrf_name, &serv->table_id);
+			strncpy(serv->vrf_name, vrf_name, IFNAMSIZ);
+		} else {
+			serv->table_id = RT_TABLE_MAIN;
+			serv->vrf_name[0] = '\0';
+		}
+#endif /* HAVE_VRF */
 		if ((opt_shared && !serv->opt_shared) || (!opt_shared && serv->opt_shared)) {
 			ipoe_drop_sessions(serv, NULL);
 			serv->opt_shared = opt_shared;
@@ -3266,6 +3278,16 @@ static void add_interface(const char *ifname, int ifindex, const char *opt, int 
 	serv->parent_ifindex = parent_ifindex;
 	serv->parent_vid = parent_ifindex ? iplink_vlan_get_vid(parent_ifindex, NULL) : 0;
 	serv->vid = vid;
+#ifdef HAVE_VRF
+	serv->vrf_ifindex = iplink_get_vrf_ifindex(ifindex);
+	if (serv->vrf_ifindex) {
+		iplink_get_vrf_info(serv->vrf_ifindex, &vrf_name, &serv->table_id);
+		strncpy(serv->vrf_name, vrf_name, IFNAMSIZ);
+	} else {
+		serv->table_id = RT_TABLE_MAIN;
+		serv->vrf_name[0] = '\0';
+	}
+#endif /* HAVE_VRF */
 	serv->active = 1;
 	INIT_LIST_HEAD(&serv->sessions);
 	INIT_LIST_HEAD(&serv->disc_list);
