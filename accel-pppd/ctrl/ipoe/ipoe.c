0a92fc28e5744b4d684ceef3a4f9a6701d21212e
Louis Scalbert <louis.scalbert@6wind.com>
2023-06-29 18:02:59 +0200

    ipoe: fix sent rebind and renew timers values
    The IPOE session lease timer value is taken from the configuration if
    available or from the LEASE_TIME macro. When the DHCP server sends an
    OFFER or an ACK with a different lease time, this value is overwritten
    but the REBIND and RENEWING timers are not changed. If the new value is
    lower than the REBIND and RENEWING timers, the clients will not go to
    the RENEWING and REBIND state to re-request its IP address before the
    lease timeout.
    
    Overwrite REBIND and RENEWING timers when receiving a lease timer from
    the server.
    
    Note the server MUST send a lease timer with a DHCP OFFER or ACK.
    
    Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

--- accel-pppd/ctrl/ipoe/ipoe.c
+++ accel-pppd/ctrl/ipoe/ipoe.c
@@ -2022,10 +2022,14 @@ static void ipoe_ses_recv_dhcpv4_relay(struct dhcpv4_packet *pack)
 	opt = dhcpv4_packet_find_opt(pack, 58);
 	if (opt)
 		ses->renew_time = ntohl(*(uint32_t *)opt->data);
+	else
+		ses->renew_time = ses->lease_time * LEASE_RENEW_TIME_RATIO;
 
 	opt = dhcpv4_packet_find_opt(pack, 59);
 	if (opt)
 		ses->rebind_time = ntohl(*(uint32_t *)opt->data);
+	else
+		ses->rebind_time = ses->lease_time * LEASE_REBIND_TIME_RATIO;
 
 	opt = dhcpv4_packet_find_opt(pack, 1);
 	if (opt)
