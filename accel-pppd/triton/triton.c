accel-ppp/accel-ppp

commit 85b204590c8d2ed028405330ca67cbd8c6dd25f9

fixed segfault at daemon termination

accel-pppd/triton/triton.c
@@ -121,8 +121,12 @@ static void* triton_thread(struct _triton_thread_t *thread)
 			} else
 				spin_unlock(&threads_lock);
 			
-			if (terminate)
+			if (terminate) {
+				spin_lock(&threads_lock);
+				list_del(&thread->entry);
+				spin_unlock(&threads_lock);
 				return NULL;
+			}
 
 			//printf("thread %p: enter sigwait\n", thread);
 			sigwait(&set, &sig);
@@ -588,7 +592,6 @@ void __export triton_run()
 void __export triton_terminate()
 {
 	struct _triton_context_t *ctx;
-	struct _triton_thread_t *t;
 	int r;
 
 	need_terminate = 1;
@@ -604,8 +607,15 @@ void __export triton_terminate()
 	}
 	spin_unlock(&ctx_list_lock);
 
-	list_for_each_entry(t, &threads, entry)
-		pthread_join(t->thread, NULL);
+	while (1) {
+		spin_lock(&threads_lock);
+		if (list_empty(&threads)) {
+			spin_unlock(&threads_lock);
+			break;
+		}
+		spin_unlock(&threads_lock);
+		sleep(1);
+	}
 	
 	md_terminate();
 	timer_terminate();

commit 6402329f5a4386cc38258c1a49ae5aa0d279e567

triton: fix a thread memory leak at exit
> ==92827==ERROR: LeakSanitizer: detected memory leaks
>
> Direct leak of 640 byte(s) in 4 object(s) allocated from:
>     #0 0x7fd6423a2867 in __interceptor_malloc ../../../../src/libsanitizer/asan/asan_malloc_linux.cpp:145
>     #1 0x7fd6422dee1f in create_thread /root/accel-ppp/accel-pppd/triton/triton.c:313
>     #2 0x7fd6422e1c0d in triton_run /root/accel-ppp/accel-pppd/triton/triton.c:768
>     #3 0x55e6ac9653bb in main /root/accel-ppp/accel-pppd/main.c:344
>     #4 0x7fd641b75d8f in __libc_start_call_main ../sysdeps/nptl/libc_start_call_main.h:58
>
> SUMMARY: AddressSanitizer: 640 byte(s) leaked in 4 allocation(s).

Free the thread memory at exit.

triton_context_unregister() sets the global terminate variable to 1 if
the global variable need_terminate is set. need_terminate is only set to
1 from triton_terminate() which is called at exit.

Fixes: 85b204590c ("fixed segfault at daemon termination")
Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

accel-pppd/triton/triton.c
@@ -163,6 +163,7 @@ static void* triton_thread(struct _triton_thread_t *thread)
 			if (terminate) {
 				spin_lock(&threads_lock);
 				list_del(&thread->entry);
+				_free(thread);
 				spin_unlock(&threads_lock);
 				return NULL;
 			}