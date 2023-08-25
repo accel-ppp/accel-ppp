accel-ppp/accel-ppp

commit 79c280518bc66560f22e9efb1d03ee1d4ff16f0b

ipoe: another fix for race during receiving relay reply

accel-pppd/ctrl/ipoe/ipoe.c
@@ -1373,6 +1373,11 @@ static void ipoe_ses_recv_dhcpv4_relay(struct dhcpv4_packet *pack)
 	if (ses->dhcpv4_relay_reply)
 		dhcpv4_packet_free(ses->dhcpv4_relay_reply);
 	
+	if (!ses->dhcpv4_request) {
+		ses->dhcpv4_relay_reply = NULL;
+		return;
+	}
+
 	ses->dhcpv4_relay_reply = pack;
 
 	if (conf_verbose) {

commit acd5e9189588af100e0ced2a2b81231c8dd900a1

ipoe: fix a relay packet memory leak when no found client packet
pack is freed at the end of ipoe_ses_recv_dhcpv4_relay()

Do it as well if the function returns earlier.

Fixes: 79c280518b ("ipoe: another fix for race during receiving relay reply")
Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

accel-pppd/ctrl/ipoe/ipoe.c
@@ -2008,6 +2008,7 @@ static void ipoe_ses_recv_dhcpv4_relay(struct dhcpv4_packet *pack)
 
 	if (!ses->dhcpv4_request) {
 		ses->dhcpv4_relay_reply = NULL;
+		dhcpv4_packet_free(pack);
 		return;
 	}
 