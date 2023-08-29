accel-ppp/accel-ppp

commit c19c04567bbe0bcd6631485795311cf83a07e5fc

ipoe: update xid of session
Signed-off-by: Dmitry Kozlov <xeb@mail.ru>

accel-pppd/ctrl/ipoe/ipoe.c
@@ -1121,7 +1121,7 @@ static void ipoe_ses_recv_dhcpv4(struct dhcpv4_serv *dhcpv4, struct dhcpv4_packe
 		triton_context_call(ses->ctrl.ctx, (triton_event_func)__ipoe_session_terminate, &ses->ses);
 		return;
 	}
-			
+
 	if (pack->msg_type == DHCPDISCOVER) {
 		if (ses->yiaddr) {
 			if (ses->serv->dhcpv4_relay) {
@@ -1131,6 +1131,7 @@ static void ipoe_ses_recv_dhcpv4(struct dhcpv4_serv *dhcpv4, struct dhcpv4_packe
 				dhcpv4_send_reply(DHCPOFFER, dhcpv4, pack, ses->yiaddr, ses->siaddr, ses->router, ses->mask, ses->lease_time, ses->dhcpv4_relay_reply);
 		}
 	} else if (pack->msg_type == DHCPREQUEST) {
+		ses->xid = pack->hdr->xid;
 		if (pack->hdr->ciaddr == ses->yiaddr && pack->hdr->xid != ses->xid)
 			ses->xid = pack->hdr->xid;
 		if ((pack->server_id && (pack->server_id != ses->siaddr || pack->request_ip != ses->yiaddr)) ||
@@ -1322,9 +1323,8 @@ static void __ipoe_recv_dhcpv4(struct dhcpv4_serv *dhcpv4, struct dhcpv4_packet 
 				triton_context_call(&opt82_ses->ctx, (triton_event_func)__ipoe_session_terminate, &opt82_ses->ses);
 			}
 		} else {
-			if (pack->hdr->ciaddr == ses->yiaddr && pack->hdr->xid != ses->xid)
-				ses->xid = pack->hdr->xid;
-			
+			ses->xid = pack->hdr->xid;
+
 			if ((pack->server_id && (pack->server_id != ses->siaddr || pack->request_ip != ses->yiaddr)) ||
 				(pack->hdr->ciaddr && (pack->hdr->xid != ses->xid || pack->hdr->ciaddr != ses->yiaddr)) ||
 				(opt82_ses && ses != opt82_ses) || (!opt82_ses && pack->relay_agent)) {
@@ -1366,6 +1366,7 @@ static void __ipoe_recv_dhcpv4(struct dhcpv4_serv *dhcpv4, struct dhcpv4_packet 
 	} else if (pack->msg_type == DHCPDECLINE || pack->msg_type == DHCPRELEASE) {
 		ses = ipoe_session_lookup(serv, pack, &opt82_ses);
 		if (ses) {
+			ses->xid = pack->hdr->xid;
 			dhcpv4_packet_ref(pack);
 			triton_context_call(&ses->ctx, (triton_event_func)ipoe_session_decline, pack);
 		}

commit b639ee513266e2266c9b99fd36d4503f569b7585

ipoe: remove dead code in ipoe_ses_recv_dhcpv4()
Remove some dead code in ipoe_ses_recv_dhcpv4. Code is now consistent
with ipoe_ses_recv_dhcpv4_request.

Fixes: c19c04567b ("ipoe: update xid of session")
Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

accel-pppd/ctrl/ipoe/ipoe.c
@@ -1604,10 +1604,8 @@ static void ipoe_ses_recv_dhcpv4(struct dhcpv4_serv *dhcpv4, struct dhcpv4_packe
 		}
 	} else if (pack->msg_type == DHCPREQUEST) {
 		ses->xid = pack->hdr->xid;
-		if (pack->hdr->ciaddr == ses->yiaddr && pack->hdr->xid != ses->xid)
-			ses->xid = pack->hdr->xid;
 		if ((pack->server_id && (pack->server_id != ses->siaddr || pack->request_ip != ses->yiaddr)) ||
-			(pack->hdr->ciaddr && (pack->hdr->xid != ses->xid || pack->hdr->ciaddr != ses->yiaddr))) {
+			(pack->hdr->ciaddr && pack->hdr->ciaddr != ses->yiaddr)) {
 
 			if (pack->server_id == ses->siaddr)
 				dhcpv4_send_nak(dhcpv4, pack, "Wrong session");